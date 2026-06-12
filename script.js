71b1877c9c7f652fd76a5b7881f2afd8d1e3864c
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-06-12 14:37:46 +0500

    start
    start

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs"); // configure ejs
+
+app.use(express.static('./public')); // express static file setup
+
 app.use(function (req, res, next){
     console.log("middleware working");
     next();
 });
 
+
+
 app.get("/", function (req, res) {
-    res.send("hello");
+    res.render("index"); // ejs bnany ke baad res.send ko res.render se replace krna ha 
+    // render krty hui views folder ke andar ki kisi file ka name likhy or eske sath .ejs na likhy
 });
 
 app.get("/profile", function (req, res) {
-    res.send("hello from profile");
+    res.render("profile");
 });
 
 app.get("/profile/:username", function (req, res) { // is main jo : ke baad likha ha wo params kehlata ha
     res.send(`hello from profile ${req.params.username}`); // url se kuch bhi acess krna ha tu wo milega req se. 
 });
 
+app.get('/error', function(req, res, next){
+    throw Error("Somthing went wrong");
+})
+
+// Error handling
+app.use(function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('error', { error: err });
+}); ///...........
 
 app.listen(3000);
